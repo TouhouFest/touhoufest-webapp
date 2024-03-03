f90a1907c10e505a1c66bed29d076d84244bd357
Brian Lee <blee025432@gmail.com>
2024-03-03 15:07:56 -0500

    add caption for google maps

--- src/menupages/AboutCon.js
+++ src/menupages/AboutCon.js
@@ -37,6 +37,7 @@ export const aboutConPage = {
           <Ratio aspectRatio="16x9">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3313.9497761747903!2d-118.34651282403861!3d33.83940582893829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b52a55535501%3A0xbf414574f90a0717!2sTorrance%20Cultural%20Arts%20Center!5e0!3m2!1sen!2sus!4v1709492414671!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </Ratio>
+          <p class="small mb-0"><a href ="https://www.google.com/maps?ll=33.839401,-118.343938&z=15&t=m&hl=en&gl=US&mapclient=embed&cid=13781372703427135255" target="_blank" rel="noreferrer">Google Maps Link (if embed isn't working)</a></p>
        </div>
        <p className="mt-3">
        A Touhou fan event being held on April 27-28th, 2024, at the Torrance Cultural Arts Center in Torrance, California. Touhou fans from across the country will gather to celebrate the coming season and enjoy all the Touhou community has to offer such as art, music, and more!
