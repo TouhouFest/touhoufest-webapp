e8d19dc7d5966740fc2c7908377ec4d033bf6347
Brian Lee <blee025432@gmail.com>
2024-04-02 22:37:31 -0400

    first test with pwa?

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 /* IMPORT YOUR MENUPAGES HERE */
 import {conCenterPage} from "./menupages/ConCenter";
@@ -31,3 +32,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
