108e1104df42724385992217bb3ca69186b70124
Brian Lee <blee025432@gmail.com>
2024-04-03 23:01:28 -0400

    add apple

--- src/menupages/Installation.js
+++ src/menupages/Installation.js
@@ -7,7 +7,7 @@ fontawesome and bootstrap are imported here for you so you can use them outright
 */
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { faArrowUpFromBracket, faDownload, faEllipsisVertical, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import { faAndroid, faChrome, faApple, faFirefoxBrowser } from '@fortawesome/free-brands-svg-icons';
 import { Image } from 'react-bootstrap';
 import Ratio from 'react-bootstrap/Ratio';
@@ -16,6 +16,7 @@ import Col from 'react-bootstrap/Col';
 import androidchrome01 from "./../images/installation/androidchrome01.jpg";
 import androidchrome02 from "./../images/installation/androidchrome02.jpg";
 import androidchrome03 from "./../images/installation/androidchrome03.jpg";
+import ios from "./../images/installation/ios.webp";
 import Accordion from 'react-bootstrap/Accordion';
 
 export const installationPage = {
@@ -45,10 +46,24 @@ export const installationPage = {
         <Accordion.Item eventKey="1">
           <Accordion.Header><FontAwesomeIcon icon={faFirefoxBrowser} className="me-1"/> Firefox</Accordion.Header>
           <Accordion.Body>
+            <ol>
+              <li>Tap the <FontAwesomeIcon icon={faEllipsisVertical}/> button to bring up additional options; it should reside on the upper right portion of your window</li>
+              <li>Tap the option named <b>Install</b></li>
+              <li>The below prompt (or something similar) should appear. Tap the <b>Add</b> button</li>
+              <li>At this point, the TouhouFest icon should appear on your phone's home screen. Tap it and it will open and function just like a regular app</li>
+            </ol>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
-      <h5><FontAwesomeIcon icon={faApple} className="mt-2"/> iOS</h5>
+      <h5><FontAwesomeIcon icon={faApple} className="mt-2"/> Apple</h5>
+      <p className="small"><FontAwesomeIcon icon={faExclamationCircle}/> Progressive Web Apps on iOS can only be installed on <b>Safari</b> at this time due to OS limitations</p>
+      <Image src={ios} fluid rounded className="mb-2"/>
+      <ol>
+        <li>Tap the <FontAwesomeIcon icon={faArrowUpFromBracket}/> (approximation) Share button </li>
+        <li>Within the prompted menu, tap the <b>Add to Home Screen</b> option</li>
+        <li>Another prompt will appear to customize the schedule's appearance. Tap the <b>Add button</b></li>
+        <li>At this point, the TouhouFest icon should appear on your phone's home screen. Tap it and it will open and function just like a regular app</li>
+      </ol>
    </>
   ),
 }
