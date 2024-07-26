cf269376d39792a2556b2822bd5445165774ab10
Brian Lee <blee025432@gmail.com>
2024-07-25 22:50:45 -0400

    very basic and probably broken tamagui setup?

---
+++ metro.config.js
@@ -0,0 +1,19 @@
+// copied from https://tamagui.dev/docs/guides/metro#web-support
+
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+})
+
+// add nice web support with optimizing compiler + CSS extraction
+const { withTamagui } = require('@tamagui/metro-plugin')
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+})
+
