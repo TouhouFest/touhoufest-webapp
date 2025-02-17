1a73748f5508a815941560f2d2b1be88bf45998d
Brian Lee <blee025432@gmail.com>
2025-02-17 17:37:13 -0500

    test secrets encryption

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
 import type { CapacitorConfig } from '@capacitor/cli';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: CapacitorConfig = {
   appId: 'com.example.touhoufestwebapp',
   appName: 'TouhouFest',
-  webDir: 'build'
+  webDir: 'build',
+  android: {
+    buildOptions: {
+      keystorePath: process.env.KEYSTOREPATH, 
+      keystorePassword: process.env.KEYSTOREPASSWORD,
+      keystoreAlias: process.env.KEYSTOREALIAS,
+      keystoreAliasPassword: process.env.KEYSTOREALIASPASSWORD
+    }
+  }
 };
 
 export default config;
