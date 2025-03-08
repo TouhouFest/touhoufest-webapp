4b4ff740b0b15b2e7f6725715d556783f7239d16
Brian Lee <blee025432@gmail.com>
2025-03-08 16:10:22 -0500

    switch to auto generated manifest

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,25 @@ export default defineConfig(() => {
     },
     plugins: [react(), commonjs(), VitePWA({
       registerType: 'autoUpdate',
-      manifest: false,
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'TouhouFest 2024 Schedule',
+        short_name: 'TouhouFest',
+        description: 'Digital schedule for TouhouFest with events, bookmarks, filters, and more',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     }) ],
     assetsInclude: ["**/*.csv"],
     base:"/"
