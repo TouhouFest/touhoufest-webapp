e7d4dd02e07582a6378b08734bd72fb67946ce3f
Brian Lee <blee025432@gmail.com>
2025-03-20 18:41:32 -0400

    devise minimum working instance of localnotifications?

--- __mocks__/@capacitor/local-notifications.ts
+++ __mocks__/@capacitor/local-notifications.ts
@@ -2,15 +2,15 @@ import { PluginListenerHandle } from "@capacitor/core/types/definitions";
 import { LocalNotificationSchema, PendingResult, PermissionStatus } from "@capacitor/local-notifications"
 import { vi } from "vitest"
 
-// vi.mock('@capacitor/local-notifications');
+vi.mock('@capacitor/local-notifications');
 
 export const LocalNotifications = {
-    async checkPermissions(): Promise<PermissionStatus>{
-        return {display:'denied'};
+    async checkPermissions(): Promise<{display: string|undefined}>{
+        return {display:undefined};
     },
 
-    async requestPermissions() : Promise<PermissionStatus> {
-        return {display:'denied'};
+    async requestPermissions() : Promise<{display: string|undefined}> {
+        return {display:undefined};
     },
 
     async addListener(eventName: 'localNotificationReceived', listenerFunc: (notification: LocalNotificationSchema) => void) {
--- src/test/IssueNotifications.test.tsx
+++ src/test/IssueNotifications.test.tsx
@@ -10,8 +10,6 @@ import { Toast } from '@capacitor/toast';
 
 describe("Notifications",() => {
     
-    const checkPermissions = vi.spyOn(LocalNotifications, "checkPermissions");
-
     test('stupid simple notifications', () => {
         let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs()}/>
         render(notification);
@@ -19,20 +17,58 @@ describe("Notifications",() => {
 
     });
 
-    test('toast fired on permissions check', async () => {
+    test('failure toast fired on permissions denied', async () => {
 
         Toast.show = vi.fn().mockReturnValue(undefined);
+        LocalNotifications.checkPermissions = vi.fn().mockImplementation(
+            async () => {
+                return {display: 'denied'}
+            }
+        );
+        LocalNotifications.requestPermissions = vi.fn().mockImplementation(
+            async() => {
+                return {display:'denied'}
+            }
+        );
 
         let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs().add(7,'day')}/>
         render(notification);
         await userEvent.click(screen.getByRole("img", {hidden:true}));
 
         await waitFor(() => {
+            expect(LocalNotifications.checkPermissions).toBeCalled();
+            expect(LocalNotifications.requestPermissions).toBeCalled();
             expect(Toast.show).lastCalledWith({
-          text:"Notifications permissions were denied!",
-          position:"center"
+                text:"Notifications permissions were denied!",
+                position:"center"
+            });
+
         });
+    });
+
+    test('default notification modal appears when not set', async () => {
+        LocalNotifications.checkPermissions = vi.fn().mockImplementation(
+            async () => {
+                return {display: 'prompt'}
+            }
+        );
+        LocalNotifications.requestPermissions = vi.fn().mockImplementation(
+            async() => {
+                return {display:'granted'}
+            }
+        );
+
+        let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs().add(7,'day')}/>
+        render(notification);
+        await userEvent.click(screen.getByRole("img", {hidden:true}));
+
+        await waitFor(() => {
+            expect(screen.getByRole("dialog")).toBeDefined();
         });
-    })
+    });
+
+    test('notifications schedule properly on timezone', async () => {
+
+    });
 });
 
