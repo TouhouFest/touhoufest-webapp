4b91cdbc85a177474bfea62da1084e100621fb64
Brian Lee <blee025432@gmail.com>
2025-03-19 23:03:12 -0400

    demonstrated minimum instances of vitest mocking capacitor

--- src/test/IssueNotifications.test.tsx
+++ src/test/IssueNotifications.test.tsx
@@ -1,4 +1,4 @@
-import { getByRole, render, screen } from '@testing-library/react';
+import { getByRole, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
 import {describe, expect, test, vi} from 'vitest';
 import IssueNotifications from '../IssueNotifications';
@@ -7,12 +7,6 @@ import userEvent from '@testing-library/user-event';
 
 import { LocalNotifications } from '@capacitor/local-notifications';
 import { Toast } from '@capacitor/toast';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 describe("Notifications",() => {
     
@@ -27,14 +21,18 @@ describe("Notifications",() => {
 
     test('toast fired on permissions check', async () => {
 
-        const showToast = vi.spyOn(Toast, "show");
+        Toast.show = vi.fn().mockReturnValue(undefined);
+
         let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs().add(7,'day')}/>
         render(notification);
         await userEvent.click(screen.getByRole("img", {hidden:true}));
 
-        expect(showToast).toHaveBeenCalled();
-        // todo: check for permissions check conducted
-        
+        await waitFor(() => {
+            expect(Toast.show).lastCalledWith({
+          text:"Notifications permissions were denied!",
+          position:"center"
+        });
+        });
     })
 });
 
