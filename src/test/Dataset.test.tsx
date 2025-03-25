897081440b0bdcee0434461f88e8e2aa67eb90d6
Brian Lee <blee025432@gmail.com>
2025-03-24 20:42:31 -0400

    begin attempts to mock events.csv

---
+++ src/test/Dataset.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, vi } from "vitest";
+
+describe("Dataset", () => {
+    test("cross-day events show proper times", async () => {
+        vi.mock("../events.csv", () => {
+
+        });
+    });
+})
