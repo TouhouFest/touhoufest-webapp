67d8b9f8b319ab74d0da15a0bc2028b15be34d05
Brian Lee <blee025432@gmail.com>
2025-04-11 06:54:47 -0400

    patch minimum notification time test

--- src/test/IssueNotifications.test.tsx
+++ src/test/IssueNotifications.test.tsx
@@ -1,16 +1,30 @@
 import { getByRole, render, screen, waitFor } from '@testing-library/react';
 import App from '../App';
-import {describe, expect, test, vi} from 'vitest';
+import {beforeAll, describe, expect, test, vi} from 'vitest';
 import IssueNotifications from '../IssueNotifications';
 import dayjs from "dayjs/esm/index.js";
 import userEvent from '@testing-library/user-event';
+import timezone from "dayjs/esm/plugin/timezone";
+import utc from "dayjs/esm/plugin/utc";
 
 import { LocalNotifications } from '@capacitor/local-notifications';
 import { Toast } from '@capacitor/toast';
 import { DEFAULTNOTIFY } from '../Utils';
 
 describe("Notifications",() => {
-   
+ 
+    // necessary to allow vitest fake timers to test properly with react-testing-library
+    beforeAll(() => {
+        const _jest = globalThis.jest;
+    
+        globalThis.jest = {
+        ...globalThis.jest,
+        advanceTimersByTime: vi.advanceTimersByTime.bind(vi)
+        };
+    
+        return () => void (globalThis.jest = _jest);
+    });
+
     test('stupid simple notifications', () => {
         let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs()}/>
         render(notification);
@@ -68,15 +82,22 @@ describe("Notifications",() => {
         });
     });
 
-    test('notifications schedule properly on timezone', async () => {
-        let collectedValues:Record<string, string>[] = [];
+    test('isolated proper time selection', async () => {
+
+        // sanity check mocking timezone
+        dayjs.extend(utc);
+        dayjs.extend(timezone);
+        vi.stubEnv("TZ","America/Los_Angeles");
+        expect(dayjs.tz.guess()).toBe("America/Los_Angeles");
+
         vi.useFakeTimers();
+
         const getItemTest = vi.spyOn(Storage.prototype, "getItem").mockImplementation((input) => {
             if(input == DEFAULTNOTIFY) {return "15";}
             else {return null;}
         });
         const setItemTest = vi.spyOn(Storage.prototype, "setItem").mockImplementation((key, value) => {
-            collectedValues.push({key: key, value: value});
+            return null
         });
         LocalNotifications.checkPermissions = vi.fn().mockImplementation(
             async () => {
@@ -93,16 +114,36 @@ describe("Notifications",() => {
         const mockedSystemTime = new Date(2025,2,3,12,30,0);
         vi.setSystemTime(mockedSystemTime);
 
-        let notification = <IssueNotifications index={1} title={"test event"} start_ts={dayjs("2025-03-21 16:00")}/>
+        let timeObject:dayjs.Dayjs = dayjs("3/21/25 16:00");
+
+        let notification = <IssueNotifications index={1} title={"test event"} start_ts={timeObject}/>
         render(notification);
+        let expectedTimeString:string = timeObject.subtract(15,'m').toISOString();
+        let expectedTimeDate:Date = timeObject.subtract(15,'m').toDate();
         await userEvent.click(screen.getByRole("img", {hidden:true}));
 
         await waitFor(() => {
-            expect(collectedValues.length).equal(2);
+            expect(setItemTest).toHaveBeenCalledWith("NOTIFY-1", expectedTimeString);
+            expect(LocalNotifications.schedule).toHaveBeenCalledWith({
+                notifications: [{
+                    title: "Event Starting Soon!",
+                    body: 'Your event "test event" is starting soon.',
+                    id: 1,
+                    schedule: {
+                        allowWhileIdle: true,
+                        at: expectedTimeDate
+                    }
+
+                }]
+            });
             // expect(LocalNotifications.schedule).toHaveBeenCalled();
-        }); /*.then(() => {
+        }).then(() => {
             vi.useRealTimers();
-        }); */
+        });
+
+    });
+
+    test("timezone-aware selection with dataset", async () => {
 
     });
 });
