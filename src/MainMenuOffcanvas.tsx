dc56e2f58fe45f5a058a2a1eed1a7fdbed0599a1
Brian Lee <blee025432@gmail.com>
2025-04-27 21:48:29 -0400

    add feedback form

--- src/MainMenuOffcanvas.tsx
+++ src/MainMenuOffcanvas.tsx
@@ -42,7 +42,7 @@ export default function MainMenuOffcanvas({mainIcon, menuheader, touhoufest, men
             <Nav className="justify-content-end flex-grow-1 pe-3">
             {menunavs}
             <Nav.Link>{darkModeSelector}</Nav.Link>
-            <Nav.Link href="https://google.com" target="_blank"><FontAwesomeIcon icon={faComment} fixedWidth/> Feedback Form</Nav.Link>
+            <Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLSeQlHDqopPY3Aw9hGe2AfEcD897pmVBKNKgjpz9Hb3sFp9hiA/viewform?usp=dialog" target="_blank"><FontAwesomeIcon icon={faComment} fixedWidth/> Feedback Form</Nav.Link>
             <Nav.Link href="https://github.com/kir12/touhoufest-webapp" target="_blank"><FontAwesomeIcon icon={faGithub} fixedWidth></FontAwesomeIcon> About App</Nav.Link>
             <Nav.Link href="https://www.google.com/search?q=marisa+kirisame&client=firefox-b-1-d&source=lnms&tbm=isch&sa=X&ved=2ahUKEwioqcvz4fT9AhW2kYkEHTCND3AQ0pQJegQIBBAC&biw=1920&bih=884&dpr=1" target="_blank"><FontAwesomeIcon icon={faHeart} fixedWidth></FontAwesomeIcon> Best Girl</Nav.Link>
             </Nav>
