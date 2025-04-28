cdb0d5b4de806287a1b6b2e9993fa3a903433ba5
Brian Lee <blee025432@gmail.com>
2025-04-27 22:08:50 -0400

    register con merch page

---
+++ src/menupages/ConMerch.tsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons';
+
+export const conMerchPage= {
+    "header": (<><FontAwesomeIcon icon={faBagShopping} fixedWidth></FontAwesomeIcon> Con Merch</>),
+    "fluidImage": (<></>),
+    "body": (<>test</>),
+}
