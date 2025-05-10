5db9809233d363b7e0a7159876572ff6d429c594
Brian Lee <blee025432@gmail.com>
2025-05-10 12:24:38 -0400

    add day of week to event description

---
+++ src/test/EventDescription.test.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, test, vi } from "vitest";
+import EventDescription from "../EventDescription";
+import { render, screen, waitFor } from "@testing-library/react";
+
+describe("EventDescription", () => {
+    test("day of week is rendered", async () => {
+        let eventdesc = <EventDescription show_var={true} hide_fxn={vi.fn()} event_package={{"daytext": "Friday, June 13", "uniqueID": 0}} evt_print={<></>}/>
+
+        render(eventdesc);
+
+        await waitFor(() => {
+            expect(screen.queryAllByText("Friday, June 13")).length.greaterThan(0);
+        });
+    }); 
+});
