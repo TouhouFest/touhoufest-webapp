84c6ec6d4bfe3f2c7ebe95ad0e54a9336b8509d5
Brian Lee <blee025432@gmail.com>
2025-05-10 22:37:30 -0400

    updated artist swap for shrug club

--- src/menupages/ArtistVendors.tsx
+++ src/menupages/ArtistVendors.tsx
@@ -76,7 +76,7 @@ import bapysociety from "./../images/artistalley/bapysociety.jpg";
 import jellistuff from "./../images/artistalley/jellistuff.jpg";
 import barksnarl from "./../images/artistalley/barksnarl.jpg";
 import girutea from "./../images/artistalley/girutea.jpg";
-import duckyafterdark from "./../images/artistalley/duckyafterdark.jpg";
+import shrugclub from "./../images/artistalley/shrugclub.jpg";
 import feeyaart from "./../images/artistalley/feeyaart.jpg";
 import melty from "./../images/artistalley/melty.jpg";
 import cynicalfries from "./../images/artistalley/cynicalfries.jpg";
@@ -154,7 +154,9 @@ let artistlist = [
 {"name": "jellisstuf", "image": jellistuff, "location": "Booth E5", "website": "https://www.etsy.com/shop/JellisStuff", "description": ""},
 {"name": "Bark Snarl", "image": barksnarl, "location": "Booth E6", "website": "https://barksnarl.myshopify.com/", "description": ""},
 {"name": "Girutea", "image": girutea, "location": "Booth F1", "website": "https://girutea.storenvy.com/", "description": ""},
-{"name": <>Duckyafter&shy;dark</>, "image": duckyafterdark, "location": "Booth F2", "website": "www.duckyafterdark.com", "description": ""},
+// fix
+{"name": "Shrug Club", "image": shrugclub, "location": "Booth F2", "website": "https://x.com/shrugclub", "description": ""},
+
 {"name": "Feeya Art", "image": feeyaart, "location": "Booth F3", "website": "https://x.com/feeyaowo", "description": ""},
 {"name": "Melty Uchuu", "image": melty, "location": "Booth F4", "website": "https://meltyportfolio.carrd.co/", "description": ""},
 {"name": "Sailin' Yumemi", "image": placeholder, "location": "Booth F5", "website": "https://space.bilibili.com/280431373?plat_id=1&share_from=space&share_medium=iphone&share_plat=ios&share_session_id=0E66CBF3-AF77-43D1-A2DA-40E600B42856&share_source=COPY&share_tag=s_i&spmid=main.space.0.0&timestamp=1721227907&unique_k=eX9r7Ab", "description": ""},
