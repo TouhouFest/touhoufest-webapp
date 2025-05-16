d0a980a58860b2fe0b98d678b0b437a8aa739f84
Brian Lee <blee025432@gmail.com>
2025-05-16 00:07:07 -0400

    change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config: CapacitorConfig = {
-  appId: 'com.example.touhoufestwebapp',
+  appId: 'com.touhou.touhoufestwebapp',
   appName: 'TouhouFest',
   webDir: 'build',
   plugins: {
