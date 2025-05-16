b8b275c96e19948696fb0d10946e0a34799ea847
Brian Lee <blee025432@gmail.com>
2025-05-16 00:10:12 -0400

    hotfix on guest vendors

--- src/menupages/Guests.tsx
+++ src/menupages/Guests.tsx
@@ -31,7 +31,7 @@ import { neosanctum,suzukann } from '../Utils';
 let djs = [
     {
         "name": "AfterGlow",
-        "image": afterglow,
+        "image": placeholder,
         "website": "https://x.com/afterglowset",
         "description": "",
         "location": "Saturday"
