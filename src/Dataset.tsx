9765b7bf4d549f0b41a1012892437bb0b974b73e
Brian Lee <blee025432@gmail.com>
2026-04-21 21:58:06 -0400

    somehow get gohei border down

--- src/Dataset.tsx
+++ src/Dataset.tsx
@@ -21,6 +21,7 @@ import noresults from './noresults.jpg';
 import noresultsdark from "./noresults-dark.jpg";
 import Stack from "react-bootstrap/Stack";
 import gohei_border from "./gohei_border.svg";
+import hakurei_border from "./hakurei_border.svg";
 import { faToriiGate, IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
 dayjs.extend(customParseFormat);
@@ -332,8 +333,14 @@ export default function Dataset(
         output.push(
           <Row className="g-0">
             <Col>
-              <ListGroup.Item className="newtimes"><h4 className="mb-0">{floortime.format("h:mm A")}</h4></ListGroup.Item>
+              <ListGroup.Item className="newtimes">
+                <h4 className="mb-0">{floortime.format("h:mm A")}</h4>
+              </ListGroup.Item>
             </Col>
+            <Col xs="auto" className="newtimes-filler text-center">
+              <Image src={hakurei_border} className="h-100 w-auto mx-auto"/>
+            </Col>
+            <Col xs="1" className="newtimes-end"></Col>
             <Col xs="auto"><Image src={gohei_border} className="h-100 filter-shadow" fluid/></Col>
           </Row>
         );
