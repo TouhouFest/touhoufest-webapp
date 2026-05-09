b0886b6e44f42998ad78ed55ff39e42af36a315f
Brian Lee <blee025432@gmail.com>
2026-05-09 14:23:31 -0400

    forgor upddate

--- src/index.jsx
+++ src/index.jsx
@@ -62,7 +62,7 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({top: 0, behavior:'instant'});
   }, [pathname]);
 
   return null;
