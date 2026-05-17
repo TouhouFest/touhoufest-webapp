3f189ed0f51335e5681d481d4ad0e3a6a1d4c773
Brian Lee <blee025432@gmail.com>
2026-05-17 16:21:07 -0400

    added jumplinks

--- src/NewMenuPage.tsx
+++ src/NewMenuPage.tsx
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
 import { MakeGoheiHeader } from "./Utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 function NewMenuPage({content}:{content:Record<string, JSX.Element>}) {
     return <>
@@ -10,6 +12,14 @@ function NewMenuPage({content}:{content:Record<string, JSX.Element>}) {
                 {!("displayTitle" in content) && 
                     <div className="mb-2"><MakeGoheiHeader content={content["header"]} vertical_margin={false} larger_header={true}/></div>
                 }
+
+                {"jumplinks" in content && <>
+                    <p>Jump directly to:</p>
+                    {content["jumplinks"].map((fragment) => {return <>
+                        <h5 className="fw-normal"><a href={`#${fragment["fragment_id"]}`} className="text-reset">{fragment["title"]} <FontAwesomeIcon icon={faAngleRight} fixedWidth/></a></h5>
+                    </>})}
+                </>}
+
                 {content["body"]}
             </div>
         </Container>
